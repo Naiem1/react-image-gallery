e44f6fd096a7d913de88f6dad29889f6b864f505
Naiem1 <naiem4288@gmail.com>
2023-11-04 10:56:44 +0600

    setup some packages and initialize project

---
+++ src/data/data.js
@@ -0,0 +1,32 @@
+// const data = [
+//   '/assets/images/image-1.webp',
+//   '/assets/images/image-2.webp',
+//   '/assets/images/image-3.webp',
+//   '/assets/images/image-4.webp',
+//   '/assets/images/image-5.webp',
+//   '/assets/images/image-6.webp',
+//   '/assets/images/image-7.webp',
+//   '/assets/images/image-8.webp',
+//   '/assets/images/image-9.webp',
+//   '/assets/images/image-10.jpeg',
+//    '/assets/images/image-11.jpeg',
+// ];
+
+// export default data;
+
+
+const data = [
+ '/assets/images/image-1.webp',
+ '/assets/images/image-2.webp',
+ '/assets/images/image-3.webp',
+ '/assets/images/image-4.webp',
+ '/assets/images/image-5.webp',
+ '/assets/images/image-6.webp',
+ '/assets/images/image-7.webp',
+ '/assets/images/image-8.webp',
+ '/assets/images/image-9.webp',
+ '/assets/images/image-10.jpeg',
+   '/assets/images/image-11.jpeg',
+];
+
+export default data;
