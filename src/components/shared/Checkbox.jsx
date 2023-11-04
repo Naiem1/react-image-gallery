9f5588fdadd89809a15ae004796b857265b737a6
Naiem1 <naiem4288@gmail.com>
2023-11-05 01:47:19 +0600

    added hover effect

--- src/components/shared/Checkbox.jsx
+++ src/components/shared/Checkbox.jsx
@@ -1,11 +1,11 @@
-const Checkbox = ({ checked, onChange }) => {
+const Checkbox = ({ checked, onChange, className }) => {
   return (
     <label className="label cursor-pointer">
       <input
         type="checkbox"
         checked={checked}
         onChange={onChange}
-        className="checkbox checkbox-primary checkbox-xs sm:checkbox-sm"
+        className={`checkbox checkbox-primary checkbox-xs sm:checkbox-sm ${className}`}
       />
     </label>
   );
